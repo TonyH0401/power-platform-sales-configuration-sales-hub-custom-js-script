2d7d8152440d0f2bb5b6b2d61be496ac61608e95
Phu Le Gia <Phu.LeGia@selfstudy.store>
2025-04-17 09:50:28 +0100

    feat: add get name

--- customButoon.js
+++ customButoon.js
@@ -3,8 +3,15 @@ var Example = window.Example || {};
   this.alertOnLoad = function (selectedControl) {
     try {
       // debugger;
-      let formContext = selectedControl;
-      console.log(selectedControl);
+      const formContext = selectedControl;
+      const selectedItems = formContext.getGrid().getSelectedRows();
+      selectedItems.forEach(function (row) {
+        const entity = row.getData().getEntity();
+        let id = entity.getId();
+        console.log("Selected record ID: " + id);
+        let name = entity.attributes.get("name")?.getValue();
+        console.log("Selected record Name: " + name);
+      });
       alert("hello world was clicked!");
     } catch (error) {
       debugger;
@@ -12,3 +19,5 @@ var Example = window.Example || {};
     }
   };
 }).call(Example);
+// https://medium.com/@furkankaracan/dynamics-365-how-to-get-row-data-from-sub-grids-ccdfbd923426
+// A combination of using ChatGPT and reading docs

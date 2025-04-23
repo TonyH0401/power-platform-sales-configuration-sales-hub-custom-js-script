c4d263957cf7d768a9c3a99514d4928cdfe91e9a
Phu Le Gia <Phu.LeGia@selfstudy.store>
2025-04-23 03:31:23 +0100

    feat: trigger Custom Button, navigate on success

--- customButoon.js
+++ customButoon.js
@@ -1,62 +1,57 @@
 var Example = window.Example || {};
 (function () {
-  this.alertOnLoad = function (
-    selectedControlSelectedItemIds,
-    selectedControl
-  ) {
+  this.alertOnLoad = function (primaryControl) {
     console.log("Hello World 2");
-    selectedControlSelectedItemIds.forEach((element) => {
-      const entityId = element;
-      const execute_crff8_BoundActionOpportunity_Request = {
-        // Parameters
-        entity: { entityType: "opportunity", id: entityId }, // entity
+    const formContext = primaryControl;
+    const entityId = formContext.data.entity.getId();
+    const execute_crff8_BoundActionOpportunity_Request = {
+      // Parameters
+      entity: { entityType: "opportunity", id: entityId }, // entity
 
-        getMetadata: function () {
-          return {
-            boundParameter: "entity",
-            parameterTypes: {
-              entity: { typeName: "mscrm.opportunity", structuralProperty: 5 },
-            },
-            operationType: 0,
-            operationName: "crff8_BoundActionOpportunity",
+      getMetadata: function () {
+        return {
+          boundParameter: "entity",
+          parameterTypes: {
+            entity: { typeName: "mscrm.opportunity", structuralProperty: 5 },
+          },
+          operationType: 0,
+          operationName: "crff8_BoundActionOpportunity",
+        };
+      },
+    };
+
+    Xrm.WebApi.execute(execute_crff8_BoundActionOpportunity_Request)
+      .then(function success(response) {
+        if (response.ok) {
+          return response.json();
+        }
+      })
+      .then(function (responseBody) {
+        const result = responseBody;
+        console.log(result);
+        // Return Type: mscrm.crff8_BoundActionOpportunityResponse
+        // Output Parameters
+        const output = result["output"]; // Edm.String
+        console.log(output);
+        if (output) {
+          const entityFormOptions = {
+            entityName: "opportunity", // Entity logical name
+            entityId: output, // The GUID returned in the response
           };
-        },
-      };
-
-      Xrm.WebApi.execute(execute_crff8_BoundActionOpportunity_Request)
-        .then(function success(response) {
-          if (response.ok) {
-            return response.json();
-          }
-        })
-        .then(function (responseBody) {
-          const result = responseBody;
-          console.log(result);
-          // Return Type: mscrm.crff8_BoundActionOpportunityResponse
-          // Output Parameters
-          const output = result["output"]; // Edm.String
-          console.log(output);
-        })
-        .catch(function (error) {
-          console.log(error.message);
-        });
-    });
-    // try {
-    //   // debugger;
-    //   const formContext = selectedControl;
-    //   const selectedItems = formContext.getGrid().getSelectedRows();
-    //   selectedItems.forEach(function (row) {
-    //     const entity = row.getData().getEntity();
-    //     let id = entity.getId();
-    //     console.log("Selected record ID: " + id);
-    //     let name = entity.attributes.get("name")?.getValue();
-    //     console.log("Selected record Name: " + name);
-    //   });
-    //   alert("hello world was clicked!");
-    // } catch (error) {
-    //   debugger;
-    //   console.error(`Error message for "alertOnLoad()": ` + error);
-    // }
+          Xrm.Navigation.openForm(entityFormOptions)
+            .then(function (result) {
+              console.log("Form opened successfully!");
+            })
+            .catch(function (error) {
+              console.log("Error opening form: " + error.message);
+            });
+        } else {
+          console.log("Invalid GUID in the response.");
+        }
+      })
+      .catch(function (error) {
+        console.log(error.message);
+      });
   };
 }).call(Example);
 // https://medium.com/@furkankaracan/dynamics-365-how-to-get-row-data-from-sub-grids-ccdfbd923426
